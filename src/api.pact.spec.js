0a5233fc78feebafa3838540141125732e88cfb5
Scottie Crump <sncrump29@gmail.com>
2023-06-13 23:17:05 -0400

    chore: add space

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -23,7 +23,7 @@ describe('API Pact test', () => {
         color: "red"
       };
 
-      // Uncomment to see this fail
+      // Uncomment to see this fail.
       // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
 
       mockProvider
